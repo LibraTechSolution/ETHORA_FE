f2cecf761a1a4970cc7423c250a84e893f046f4a
QuangKhanh <58257872+phamquangkhanh2999@users.noreply.github.com>
2023-10-07 15:20:14 +0700

    ui responsive mobile landingpage

--- src/views/HomePageView/index.tsx
+++ src/views/HomePageView/index.tsx
@@ -40,25 +40,54 @@ export const HomeView = () => {
         gap="25px"
         alignItems={'center'}
         paddingBottom={'20px'}
-        paddingTop="86px"
-        bgImage="linear-gradient(to bottom, #0f0f14eb, #0c0c10), url('/images/landingpage/bg-lading-top.gif');"
+        paddingTop={{ base: '57px', md: '86px' }}
+        bgImage={{
+          base: "linear-gradient(to bottom, #0f0f14eb, #0f0f1a), url('/images/landingpage/bg-lading-top.gif')",
+          md: "linear-gradient(to bottom, #0f0f14eb, #0c0c10), url('/images/landingpage/bg-lading-top.gif')",
+        }}
       >
         <GridItem>
-          <Box paddingLeft={'150px'} paddingRight={'80px'}>
-            <Box display={'flex'} alignItems={'center'} gap={'2'} marginBottom={'30px'}>
+          <Box paddingLeft={{ base: '12px', md: '50px', lg: '150px' }} paddingRight={{ base: '12px', md: '80px' }}>
+            <Box
+              display={'flex'}
+              alignItems={'center'}
+              gap={'2'}
+              marginBottom={{ base: '20px', md: '30px' }}
+              justifyContent={{ base: 'center', sm: 'center', md: 'start', lg: 'start' }}
+            >
               <Text as="span" fontSize={'xs'} color={'#9E9E9F'}>
                 POWERED BY
               </Text>
               <Image alt="base" src="/images/landingpage/base.png" w="24" h="5" />
             </Box>
-            <Text as="p" textColor={'white'} fontSize={'60px'} lineHeight={'70px'} marginBottom={'30px'}>
+            <Text
+              as="p"
+              textColor={'white'}
+              fontSize={{ base: '3xl', md: '30px', lg: '60px' }}
+              lineHeight={{ base: '45px', md: '40px', lg: '70px' }}
+              marginBottom={{ base: '20px', md: '30px' }}
+              textAlign={{ base: 'center', md: 'start' }}
+            >
               Decentralized <br /> Options Trading
             </Text>
-            <Text as="p" fontSize={'xs'} color={'#9E9E9F'} marginBottom={'30px'}>
+            <Text
+              as="p"
+              fontSize={'xs'}
+              color={'#9E9E9F'}
+              marginBottom={'30px'}
+              textAlign={{ base: 'center', md: 'start' }}
+            >
               Discover a non-custodial platform specialized in options trading, tailored for effectively managing
               short-term price volatility and mitigating risks associated with high-leverage positions.
             </Text>
-            <Box display={'flex'} alignItems={'center'} gap={10}>
+            <Box
+              display={'flex'}
+              alignItems={'center'}
+              gap={10}
+              paddingLeft={{ base: '12px', md: '0px' }}
+              paddingRight={{ base: '12px', md: '0px' }}
+              justifyContent={{ base: 'center', md: 'start' }}
+            >
               <Text as="span" fontSize={'xs'} textColor={'#0052FF'} cursor={'pointer'}>
                 Trade <ExternalLinkIcon />
               </Text>
@@ -71,27 +100,41 @@ export const HomeView = () => {
             </Box>
           </Box>
         </GridItem>
-        <GridItem>
+        <GridItem display={{ base: 'none', sm: 'none', md: 'block', lg: 'block' }}>
           <Image alt="trade" src="/images/landingpage/trade.png" w="full" h="full" objectFit="cover" />
+        </GridItem>
+        <GridItem display={{ base: 'block', sm: 'block', md: 'none', lg: 'none' }} margin={'0px 12px'}>
+          <Image alt="trade" src="/images/landingpage/trade_mobile.png" w="full" h="full" objectFit="cover" />
         </GridItem>
       </Grid>
       <Box
         display={'flex'}
         alignItems={'center'}
         justifyContent={'center'}
-        bgImage="url('/images/landingpage/bg-ethora.png')"
+        bgImage={"url('/images/landingpage/bg-ethora.png')"}
         bgRepeat="no-repeat"
         bgPosition={'center'}
-        bgSize={'125% 250%'}
+        bgSize={{ base: '400% 370%', sm: '400% 370%', md: '195% 295%', lg: '125% 250%' }}
         paddingTop={'20%'}
         position={'relative'}
         flexDirection={'column'}
       >
-        <Box position="absolute" top={'20px'} left={'-2'}>
+        <Box
+          position="absolute"
+          top={'20px'}
+          left={'-2'}
+          display={{ base: 'none', sm: 'none', md: 'none', lg: 'block' }}
+        >
           <Image src="/images/landingpage/ethora.png" alt="landingpage" width={'full'} height={'full'} />
         </Box>
         <Box maxWidth={'1150px'} margin={'0 auto'}>
-          <Text fontSize="56px" textColor={'white'} lineHeight={'120px'} fontWeight={600} textAlign={'center'}>
+          <Text
+            fontSize={{ base: '28px', md: '60px' }}
+            textColor={'white'}
+            lineHeight={'120px'}
+            fontWeight={600}
+            textAlign={'center'}
+          >
             What does Ethora offer?
           </Text>
           <Grid
@@ -103,6 +146,7 @@ export const HomeView = () => {
             }}
             gap="25px"
             alignItems={'center'}
+            marginLeft={{ base: '12px', sm: '12px', md: '0px', lg: '0px' }}
           >
             {dataCardEthora.map((item, idx) => (
               <CardEthora key={idx} alt={item.alt} image={item.image} title={item.title} desc={item.desc} />
@@ -117,8 +161,21 @@ export const HomeView = () => {
         bgSize={'cover'}
         paddingTop={'10%'}
       >
-        <Box position="absolute" bottom={'-140px'} left={'-2'}>
+        <Box
+          position="absolute"
+          bottom={'-140px'}
+          left={'-2'}
+          display={{ base: 'none', sm: 'none', md: 'block', lg: 'block' }}
+        >
           <Image src="/images/landingpage/bg-hexagon.png" alt="landingpage" width={'full'} height={'full'} />
+        </Box>
+        <Box
+          position="absolute"
+          bottom={'-19px'}
+          left={'-342px'}
+          display={{ base: 'block', sm: 'block', md: 'none', lg: 'none' }}
+        >
+          <Image src="/images/landingpage/ethora.png" alt="landingpage" width={'800px'} height={'800px'} />
         </Box>
         <Box
           display={'flex'}
@@ -128,7 +185,13 @@ export const HomeView = () => {
           bgPosition="center"
           flexDirection={'column'}
         >
-          <Text fontSize="56px" textColor={'white'} lineHeight={'120px'} fontWeight={600} marginBottom={'80px'}>
+          <Text
+            fontSize={{ base: '28px', md: '60px' }}
+            textColor={'white'}
+            lineHeight={'120px'}
+            fontWeight={600}
+            marginBottom={{ base: '0px', sm: '0px', md: '80px', lg: '80px' }}
+          >
             Dual Token System
           </Text>
           <Grid
@@ -141,6 +204,8 @@ export const HomeView = () => {
             gap="25px"
             zIndex={'2'}
             alignItems={'stretch'}
+            marginLeft={{ base: '12px', sm: '12px', md: '12px', lg: '0px' }}
+            marginRight={{ base: '12px', sm: '12px', md: '12px', lg: '0px' }}
           >
             <CardDualToken
               image="/images/landingpage/img_etr.png"
@@ -163,21 +228,29 @@ export const HomeView = () => {
           </Grid>
         </Box>
         <Box display={'flex'} alignItems={'center'} justifyContent={'center'} gap={'24px'} margin={'80px 0 40px'}>
-          <Link href="/" className="flex items-center gap-1 text-sm ">
+          <Link href="/" className="z-10 flex items-center gap-1 text-sm">
             <Image alt="ethora" src="/images/icons/twitter.svg" w="24px" h="24p" />
-            <span>Twitter</span>
+            <Text as={'span'} display={{ base: 'none', sm: 'none', md: 'inline-block', lg: 'inline-block' }}>
+              Twitter
+            </Text>
           </Link>
-          <Link href="/" className="flex items-center gap-1 text-sm">
+          <Link href="/" className="z-10  flex items-center gap-1 text-sm">
             <Image alt="ethora" src="/images/icons/twitter.svg" w="24p" h="24p" />
-            <span>Discord</span>
+            <Text as={'span'} display={{ base: 'none', sm: 'none', md: 'inline-block', lg: 'inline-block' }}>
+              Discord
+            </Text>
           </Link>
-          <Link href="/" className="flex items-center gap-1 text-sm">
+          <Link href="/" className="z-10  flex items-center gap-1 text-sm">
             <Image alt="ethora" src="/images/icons/medium.svg" w="24p" h="24p" />
-            <span>Medium</span>
+            <Text as={'span'} display={{ base: 'none', sm: 'none', md: 'inline-block', lg: 'inline-block' }}>
+              Medium
+            </Text>
           </Link>
-          <Link href="/" className="flex items-center gap-1 text-sm">
+          <Link href="/" className="z-10 flex items-center gap-1 text-sm">
             <Image alt="ethora" src="/images/icons/instagram.svg" w="24p" h="24p" />
-            <span>Instagram</span>
+            <Text as={'span'} display={{ base: 'none', sm: 'none', md: 'inline-block', lg: 'inline-block' }}>
+              Instagram
+            </Text>
           </Link>
         </Box>
       </Box>
